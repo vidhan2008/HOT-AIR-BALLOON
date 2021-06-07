c4155ede585b346ceef1ba06b66a5e37440a1755
vidhan2008 <79260006+vidhan2008@users.noreply.github.com>
2021-06-07 18:38:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database;
+var position
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+  textSize(20); 
+
+  database = firebase.database();
+   var positionRef= database.ref("balloon/position")
+    positionRef.on("value",readposition,showError)
+ 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    WritePosition(-1,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    WritePosition(+1,0);
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    WritePosition(0,-1);
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    WritePosition(0,+1);
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function WritePosition(x,y){
+  //ball.x = ball.x + x;
+   //ball.y = ball.y + y;
+   database.ref("balloon/position").set({
+       x:balloon.x+x,
+       y:balloon.y+y
+   })
+   
+}
+
+function readposition(data){
+position=data.val()
+balloon.x=position.x
+balloon.y=position.y
+}
+
+function showError(){
+   console.log("Error Message")
+}
